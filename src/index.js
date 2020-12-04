ec30eb931803ecc2ae7d70156cf9670c5958366f
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-03 21:40:53 -0500

    init and render

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/index.css';
 import App from '../src/App';
+
+
+// render 
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
