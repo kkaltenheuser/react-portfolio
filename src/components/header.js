99a20da6a1065be5134b2dfe747d81550016e620
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-03 20:47:08 -0500

    moved file assets for clarity and file access for header.js

---
+++ src/components/header.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap"
+import { Link, useLocation } from "react-router-dom";
+import Resume from "../file_assets/Resume2.pdf"
