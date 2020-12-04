02aee733c81eee9fb310960c3865505172e633d8
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-03 20:22:14 -0500

    foundation for index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import { TodoProvider} from "./context/Todocontext"
-
-ReactDOM.render(
-    <TodoProvider>
-        <App />
-    </TodoProvider>,
-    document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/index.css';
+import App from '../src/App';
