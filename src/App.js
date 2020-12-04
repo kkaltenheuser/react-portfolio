4da63360873553746bcc05be8209dcbb2ea5dc51
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-03 20:18:45 -0500

    clean up file structure in app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { BrowserRouter as Router, Route } from "react-router-dom"
+
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-import Home from "./components/Pages/Home.js"
-import Portfolio from "./components/Pages/Portfolio.js"
-import Contact from "./components/Pages/Contact.js"
-import Navbar from "./components/Header"
-import Footer from "./components/Footer"
+
+import Home from "./components/pages/home"
+import Portfolio from "./components/pages/portfolio"
+import Contact from "./components/pages/contact"
+import Navbar from "./components/header"
+import Footer from "./components/footer"
 
 function App() {
+  
   return (
     <Router>
+
       <div>
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} /> 
+        <Route exact path="/src/components/pages/home.js" component = { Home } />
+        <Route exact path="/src/components/pages/portfolio.js" component = { Portfolio } />
+        <Route exact path="/src/components/pages/contact.js" component=  { Contact } /> 
         <Footer />
       </div>
+
     </Router>
   );
 }
