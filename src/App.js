dfc150cceb8cbe91ed843e41d4b7f2e38deaf8c1
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-03 20:13:29 -0500

    updated code to fit project

--- src/App.js
+++ src/App.js
@@ -1,69 +1,24 @@
-import React, { useReducer, useRef } from "react";
-import "./App.css";
-import Form from "./components/form"
-import List from "./components/list"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Home from "./components/Pages/Home.js"
+import Portfolio from "./components/Pages/Portfolio.js"
+import Contact from "./components/Pages/Contact.js"
+import Navbar from "./components/Header"
+import Footer from "./components/Footer"
 
-
-function TodoList() {
-  const inputRef = useRef();
-  const [items, dispatch] = useReducer((state, action) => {
-    switch (action.type) {
-    case "add":
-      return [
-        ...state,
-        {
-          id: state.length * Math.random(),
-          name: action.name
-        }
-      ];
-      // Bonus: Remove a todo from the list.
-    case "remove":
-      return state.filter((_, index) => {
-        return index !== action.index;
-      });
-    default:
-      return state;
-    }
-  }, []);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    dispatch({
-      type: "add",
-      name: inputRef.current.value
-    });
-    inputRef.current.value = "";
-  }
-
+function App() {
   return (
-    <div className="container text-center">
-      <h1>Create a Todo List!</h1>
-      <form className="form-group mt-5" onSubmit={handleSubmit}>
-        <input
-          className="form-control"
-          ref={inputRef}
-          placeholder="Start typing what you need to do..."
-        />
-        <button className="btn btn-success mt-3 mb-5" type="submit">
-          Add to List
-        </button>
-      </form>
-      <h4>My Todo List:</h4>
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li className="list-group-item" key={item.id}>
-            {item.name}{" "}
-            <button
-              className="btn btn-danger ml-5"
-              onClick={() => dispatch({ type: "remove", index })}
-            >
-              X Remove
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} /> 
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
-export default TodoList;
+export default App;
