c45c8edf9b343f8a5586b0835e84c9c194b5e90b
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-07 00:08:16 -0500

    cleaned up footer for user viewing

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,35 +1,11 @@
-import React from "react";
+import React from 'react';
 
-function Footer() {
-    
-    return (
-        <footer class="jumbotron jumbotron-fluid fixed-bottom mb-0">
-            <div class="container">
-                <div id="footer-row" class="row">
-                    <div class="col-3 col-sm-3 col-md-4 col-lg-5"></div>
-                    <div class="col-9 col-sm-9 col-md-8 col-lg-7">
-                        Coded by Ren
-                    </div>
-                    <div class="col-3 col-sm-3 col-md-4 col-lg-5"></div>
-                    <div class="col-9 col-sm-9 col-md-8 col-lg-7">
-                        <a target="_blank" href="https://github.com/kkaltenheuser">
-                        <i class="fab fa-github-square"></i>
-                        </a>
-                        <a
-                        class="ml-4"
-                        target="_blank"
-                        href="https://linkedin.com/in/karenkaltenheuser"
-                        >
-                        <i class="fab fa-linkedin"></i>
-                        </a>
-                        <a class="ml-4" href="mailto:karenkaltenheuser@gmail.com">
-                        <i class="fas fa-envelope-square"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+const Footer = () => {
+  return (
+    <footer className="stickfoot text-center">
+      <h6 className="copy">&copy; K. Kaltenheuser </h6>
+    </footer>
+  )
 }
 
 export default Footer
