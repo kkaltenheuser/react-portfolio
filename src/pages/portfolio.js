c29adfd57a411c1f970b14fbc2cf2e86628beba0
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-07 00:43:48 -0500

    updated paths and names

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Project from "../components/Project/index.js";
-import "" from "";
-import "" from "";
-import "" from "";
-import "" from "";
-import "" from "";
-import "" from "";
+import Project from "../components/project/index";
+import GetMeOutOfHere from "../images/get-outta-here-illustration.png";
+import JSQuiz from "../images/JS-quiz.png";
+import NoteTaker from "../images/note-taker.png";
+import PasswordGenerator from "../images/Password-Generator.png";
+import RememberDat from "../images/rememberdatlogo.png";
+import WorkDayScheduler from "../images/workdayscheduler.png";
 
