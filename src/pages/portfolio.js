2b4215489e2d9cab690a9411a80a164b4618d98f
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-07 00:34:32 -0500

    foundation for six featured projects

---
+++ src/pages/portfolio.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Project from "../components/Project/index.js";
+import "" from "";
+import "" from "";
+import "" from "";
+import "" from "";
+import "" from "";
+import "" from "";
+
