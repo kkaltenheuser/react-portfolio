4d4e1c1548c094eb368611880601e135e7fd1621
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-06 23:59:25 -0500

    fleshed out navigation bar for header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,48 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap"
-import { Link, useLocation } from "react-router-dom";
-import Resume from "../file_assets/Resume2.pdf"
+
+const Header = () => {
+    return (
+      // Navigation Bar
+      <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+
+           {/*<!-- nav - toggle collapse for mobile --> */}     
+          <div className="navbar-header">
+            <button
+              type="button"
+              className="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#bs-example-navbar-collapse-1"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="/index">About Me</a>
+          </div>
+  
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li className="active">
+                <a href="/portfolio"
+                  >Portfolio <span className="sr-only">(current)</span></a
+                >
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+              <li>
+                <a href="/socials">Socials</a>
+              </li>  
+            </ul>
+          </div>
+          {/* <!-- /.navbar-collapse location end --> */}
+        </div>
+        {/* <!-- /.container-fluid location end --> */}
+      </nav>
+    )
+  }
+  
+  export default Header
